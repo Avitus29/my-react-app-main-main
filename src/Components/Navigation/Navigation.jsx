783b183127825b74d4ba8a490fcde85c26abab61
Avitus Valence <avitusvalence6@gmail.com>
2025-04-15 21:38:42 +0300

    Refactor Navigation component structure and styles for improved clarity and UX

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,17 +1,18 @@
-import "./Navigation.css"
+import "./Navigation.css";
+
 const Navigation = () => {
   return (
     <div>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>Appointments</li>
-          <li>About</li>
-          <li>Contact</li>
+      <nav className="navigation">
+        <ul className="menu">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#appointments">Appointments</a></li>
+          <li><a href="#about">About</a></li> {/* Fixed closing tag */}
+          <li><a href="#contact">Contact</a></li> {/* Fixed closing tag */}
         </ul>
       </nav>
     </div>
   )
 }
 
-export default Navigation
+export default Navigation;
