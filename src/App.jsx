431cae640592959bd9a81c1de4d67a5c202cead8
Avitus Valence <avitusvalence6@gmail.com>
2025-04-16 23:22:18 +0300

    Add react-router-dom dependency to package.json and package-lock.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Content from "./Components/Content/Content.jsx"
 import Footer from "./Components/Footer/Footer.jsx"
 import Header from "./Components/Header/Header.jsx"
 import Navigation from "./Components/Navigation/Navigation.jsx"
+import "./App.css";
+import Appointment from "./Components/Appointment/Appointment";
+
 
 const App = () => {
   return (
     <div>
-      <Navigation />
-      <Header />
-      <Content />
-      <Footer />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Content />} />
+          <Route path="/appointment" element={<Appointment />} />
+        </Routes>
+      </Router>
+      <div>
+        <Navigation />
+        <Header />
+        <Content />
+        <Footer />
+      </div>
     </div>
   )
 }
